21229aac93c8a4d2024364f8fd24a9925ffd9206
Zoltan Csilleri <zcsilleri@gmail.com>
2022-03-25 19:11:53 +0100

    wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 import type { NextPage } from 'next';
+import { Form } from '../shared/form/form';
 import { BaseLayout } from '../shared/layouts/base.layout';
 
 const Home: NextPage = () => {
     return <BaseLayout auth={false} meta={{ title: 'home page', keywords: 'whatever', description: 'some description' }}>
         <h1 className={'fs-55'}> its my home page</h1>
+
+        <Form onSubmit={() => console.log('submitting')} isLoading={false} error={''} clearError={} successMessage={} clearMessage={} client={}>
+
+        </Form>
     </BaseLayout>;
 };
 
--- shared/form/form.tsx
+++ shared/form/form.tsx
@@ -6,56 +6,24 @@ import { ErrorModal } from './error-modal';
 import Input from './input';
 
 interface FormProps extends ClientProps {
-    onSubmit: (form: any) => void;
-    form: any;
+    onSubmit: () => void;
     submitButton?: ButtonProps;
     className?: string;
     onSuccess?: () => void;
     onError?: () => void;
+    children: any;
 }
 
-const getIsFormValid = (form: any) => {
-    const isValidArr = [];
-    for (const prop in form) {
-        if (form.hasOwnProperty(prop)) {
-            if (!form[prop]?.isValid) {
-                return false;
-            }
-            isValidArr.push(`${form[prop]?.isValid}`);
-        }
-    }
-
-    return !isValidArr.includes('false');
-};
-
 export const Form = (props: FormProps) => {
-    const [form, setForm] = useState({});
-    const [isFormValid, setIsFormValid] = useState(false);
     const { isLoading, error, clearError, successMessage, clearMessage } = props;
-    useEffect(() => {
-        setIsFormValid(getIsFormValid(form));
-    }, [getIsFormValid, form]);
-
-    const getData = (data: any, isValid: boolean) => {
-        setForm({ ...form, [data?.name]: { value: data?.value, isValid: isValid } });
-    };
-
-    const getRestructureForm = (form: any) => {
-        const restructuredForm: any = {};
-        for (const prop in form) {
-            if (form.hasOwnProperty(prop)) {
-                restructuredForm[prop] = form[prop]?.value || null;
-            }
-        }
-
-        return restructuredForm;
-    };
-
+    // TODO -> we will need the context data to come here with form validity and such
+    const isFormValid = true;
+    // perhaps get the form data here and submit it too. see how it goes
     const submit = async (e: any) => {
         e.preventDefault();
-        const response: any = await props.onSubmit(getRestructureForm(form));
+        props.onSubmit();
     };
-
+    // review this logic
     const manageSuccessClose = () => {
         if (props.onSuccess) {
             props.onSuccess();
@@ -77,13 +45,7 @@ export const Form = (props: FormProps) => {
             onSubmit={(e) => submit(e)}
             className={props.className}
         >
-            {props.form?.fields && props.form.fields.map((field: any, index: number) => {
-                return <Input
-                    {...field}
-                    key={field.name}
-                    getData={getData}
-                />;
-            })}
+            {props.children}
             <div className={'row'}>
                 {props.submitButton && <Button
                     isLoading={isLoading}
