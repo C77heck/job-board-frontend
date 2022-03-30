69e6c0153d4d2e990dad5a90103cc563cc9af0b6
Zoltan Csilleri <zcsilleri@gmail.com>
2022-03-30 18:33:56 +0200

    wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,12 @@
 import type { NextPage } from 'next';
+import { Header } from '../components/HomeScreen/Components/header';
 import Search from '../components/HomeScreen/Components/search';
-import { Field } from '../shared/form/field';
-import { Form } from '../shared/form/form';
-import { FormStructure } from '../shared/form/form.structure';
-import { Input } from '../shared/form/input';
-import { requiredValidator } from '../shared/form/validators/required-validator';
-import { useClient } from '../shared/hooks/client';
 import { BaseLayout } from '../shared/layouts/base.layout';
 
 const Home: NextPage = () => {
 
     return <BaseLayout auth={false} meta={{ title: 'home page', keywords: 'whatever', description: 'some description' }}>
-        <div className={'w-100 position-center'}>
-            <Search/>
-        </div>
+        <Header><Search/></Header>
     </BaseLayout>;
 };
 
