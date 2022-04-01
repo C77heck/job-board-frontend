47c9bd02b01efe0dba8ea13f78f14ba2de8f7faf
Zoltan Csilleri <zcsilleri@gmail.com>
2022-04-01 08:08:24 +0200

    wip

--- components/AdsListScreen/Components/job-card.tsx
+++ components/AdsListScreen/Components/job-card.tsx
@@ -13,9 +13,9 @@ export const JobCard = ({ title, date, description, location, salary, className 
             <h2 className={'fs-19 fw--700 pb-3 text-color--secondary-1'}>{title}</h2>
             <Button title={<FavouriteIcon width={25} className={'text-color--dark hover-secondary'}/>} buttonStyle={'transparent'}/>
         </div>
-        <h2 className={'fs-16 fw--700'}>{salary}</h2>
-        <h2 className={'fs-16 fw--400'}>{date}</h2>
+        <h2 className={'fs-17 fw--700'}>{salary}</h2>
+        <h2 className={'fs-15 fw--400'}>{date}</h2>
         <p className={'fs-15 fw--500'}>{location}</p>
-        <p className={'fs-12 fw--400 pt-16'}>{formatLongText(description, 420)}</p>
+        <p className={'fs-13 fw--400 pt-16'}>{formatLongText(description, 420)}</p>
     </div>;
 };
