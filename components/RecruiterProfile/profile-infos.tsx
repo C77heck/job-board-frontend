c3df499e1e42e8be7305ca4a053c91861f1fcdf3
Zoltan Csilleri <csilleri.zoltan@codebuild.hu>
2022-07-29 08:59:39 +0200

    wip

--- components/RecruiterProfile/profile-infos.tsx
+++ components/RecruiterProfile/profile-infos.tsx
@@ -1,18 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { BoxWrapper } from '../../shared/components/ui-misc/box-wrapper';
 import { DataPresenter } from '../../shared/components/ui-misc/data-presenter';
-import { useAuth } from '../../shared/hooks/auth-hook';
+import { AuthContext } from '../../shared/contexts/auth.context';
+import { UserMeta } from '../../shared/hooks/auth-hook';
 import { ProfileItem } from '../JobSeekerProfileScreen/libs/user.data.document';
 import { RecruiterInfoForm } from './forms/recruiter-info.form';
 
 export const ProfileInfos = (props: any) => {
-    const { userData } = useAuth();
+    const { userData } = useContext(AuthContext);
     const [formattedUserData, setFormattedUserData] = useState<any[]>([]);
 
     useEffect(() => {
+        console.log({ userData }, Object.keys(userData), Object.keys(userData).map((prop: string) => ({ [prop]: userData[prop as keyof UserMeta] || '' })));
         if (!!userData) {
-            // @ts-ignore
-            setFormattedUserData(Object.keys(userData).map((prop: string) => ({ [prop]: userData[prop] || '' })));
+            setFormattedUserData(Object.keys(userData).map((prop: string) => ({ label: prop, data: userData[prop as keyof UserMeta] || '' })));
         }
     }, [userData]);
 
