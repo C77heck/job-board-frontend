98f241857e8cfc96c0633f8552c250a13e86557d
Zoltan Csilleri <csilleri.zoltan@codebuild.hu>
2022-08-03 10:09:36 +0200

    wip

--- shared/components/navigation/login.button.tsx
+++ shared/components/navigation/login.button.tsx
@@ -3,11 +3,9 @@ import { useContext, useState } from 'react';
 import { AuthContext } from '../../contexts/auth.context';
 import { Button } from '../buttons/button';
 import { LoginIcon } from '../icons/icons';
-import { Modal } from '../modal/modal';
 import { EmployeeDropdown } from './employee-dropdown';
 import { ProfileDropdown } from './libs/profile-dropdown';
-import { LoginForm } from "./forms/login.form";
-import { RegisterForm } from "./forms/register.form";
+import { LoginTypeDropdown } from './login-type.dropdown';
 import { RecruiterDropdown } from './recruiter-dropdown';
 
 export const LoginButton = ({ isMobile }: any) => {
@@ -36,19 +34,5 @@ export const LoginButton = ({ isMobile }: any) => {
         />;
     }
 
-    return <Modal
-        level={2}
-        className={'border-radius-px-5 p-15'}
-        content={isRegister
-            ? <RegisterForm endpoint={'users/recruiter/signup'} onClick={() => setIsRegister(false)}/>
-            : <LoginForm endpoint={'users/recruiter/login'} onClick={() => setIsRegister(true)}/>}
-        size={{ sm: 90, md: 67, lg: 50, xl: 40 }}
-        header={<h2 className={'header--3 text-align-center'}>{isRegister ? 'Sign up' : 'Sign in'}</h2>}
-        wrapperClass={'align-self-center'}
-        trigger={<Button
-            textColor={'text-color--light-1'}
-            buttonStyle={'transparent'}
-            title={<LoginIcon width={24} className={'text-color--light-1 pt-3'}/>}
-        />}
-    />;
+    return <LoginTypeDropdown/>;
 };
