542d0dd8970ab900ebf0266d001fe1bea5d62368
Zoltan Csilleri <csilleri.zoltan@codebuild.hu>
2022-08-15 15:50:45 +0200

    wip

--- shared/components/navigation/libs/analyitics.tsx
+++ shared/components/navigation/libs/analyitics.tsx
@@ -1,13 +1,17 @@
 import { useContext, useEffect } from 'react';
 import { AuthContext } from '../../../contexts/auth.context';
+import { SessionContext } from '../../../contexts/session.context';
 import { useClient } from '../../../hooks/client';
 
 export const Analyitics = () => {
     const { isLoggedIn } = useContext(AuthContext);
+    const { sessionId, setNewSessionId } = useContext(SessionContext);
+
     const { client, error, } = useClient();
     const getSessionId = async () => {
         try {
             const response = await client('/analytics');
+            setNewSessionId(response.sessionId);
             console.log({ response });
         } catch (e) {
             console.log(e, error);
--- shared/contexts/wrappers/sesssion-context.wrapper.tsx
+++ shared/contexts/wrappers/sesssion-context.wrapper.tsx
@@ -3,6 +3,7 @@ import { SessionContext } from '../session.context';
 
 export const SesssionContextWrapper = (props: any) => {
     const session = useSession();
+
     return <SessionContext.Provider value={session}>
         {props.children}
     </SessionContext.Provider>;
